4454fac9e8c1ba18d412250ef07e3b7791c10426
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-04-24 15:02:27 +0000

    Build the navigate to prev page button

--- src/pages/CountryDetailsPage.tsx
+++ src/pages/CountryDetailsPage.tsx
@@ -1,24 +1,35 @@
+import { Button } from "@/components/ui/button";
 import CountryDetails from "@/components/ui/CountryDetails";
 import useCountries from "@/hooks/useCountries";
-import { useParams } from "react-router-dom";
-
+import { ArrowLeft } from "lucide-react";
+import { Link, useParams } from "react-router-dom";
 
 const CountryDetailsPage = () => {
-  const {id } = useParams();
-  const {countries} = useCountries();
-  const filtered = countries.filter(c => c.name === id )
-  const country = filtered[0]
- 
-  return (
-    <div className="flex flex-col md:flex-row justify-between gap-2 w-[80vw] mx-auto">
-      <div className="w-[50%] md:w-[450px]">
-        <img src={country.flag!} alt="" className="" />
-      </div>
+  const { id } = useParams();
+  const { countries } = useCountries();
+  const filtered = countries.filter((c) => c.name === id);
+  const country = filtered[0];
 
-      <div className="w-[50%]">
-        <CountryDetails country={country} />
+  return (
+    <>
+      <div className="mb-10 w-[80vw] mx-auto mt-5">
+        <Link to={"/"}>
+          <Button variant={"outline"} className="text-gray-700">
+            <ArrowLeft className="w-10 h-5" />
+            Back
+          </Button>
+        </Link>
       </div>
-    </div>
+      <div className="flex flex-col md:flex-row justify-between gap-2 w-[80vw] mx-auto">
+        <div className="w-[50%] md:w-[450px]">
+          <img src={country.flag!} alt="" className="" />
+        </div>
+
+        <div className="w-[50%]">
+          <CountryDetails country={country} />
+        </div>
+      </div>
+    </>
   );
 };
 export default CountryDetailsPage;
