a90f5a981d76f5d4e84290067b65e0914a9ebddd
Nurudeen Hamzah <hamdeen247@gmail.com>
2024-04-25 07:45:44 +0000

    remove the search button in the search bar

--- src/components/ui/SearchCountryBar.tsx
+++ src/components/ui/SearchCountryBar.tsx
@@ -50,12 +50,6 @@ const SearchCountryBar = ({ onQueryCountry }: Props) => {
             handleQueryCountry(e);
           }}
         />
-        <button
-          type="submit"
-          className="text-white absolute end-2.5 bottom-2.5 bg-blue-400 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Search
-        </button>
       </div>
     </form>
   );
